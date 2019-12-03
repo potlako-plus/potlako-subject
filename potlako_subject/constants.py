19a22125aeb3309bfde9b753997064a10831c079
Ame <adiphoko@bhp.org.bw>
2019-12-03 11:39:18 +0200

    potlako constants

---
+++ potlako_subject/constants.py
@@ -0,0 +1,17 @@
+CLINICIAN = (
+    ('Medical Officer', 'Medical Officer'),
+    ('Specialist - Family Medicine', 'Specialist - Family Medicine'),
+    ('Specialist - Internal Medicine', 'Specialist - Internal Medicine'),
+    ('Specialist - General Surgeon', 'Specialist - General Surgeon'),
+    ('Specialist - Ob/GYN (Gynecologist)', 'Specialist - Ob/GYN (Gynecologist)'),
+    ('Specialist - Oncologist', 'Specialist - Oncologist'),
+    ('Specialist - Pathologist', 'Specialist - Pathologist'),
+    ('Specialist - Hematologist', 'Specialist - Hematologist'),
+    ('Specialist - Palliative care', 'Specialist - Palliative care'),
+    ('Doctor - Other type (specify)', 'Doctor - Other type (specify)')
+    ('Nurse - FNP', 'Nurse - FNP'),
+    ('Nurse - Midwife', 'Nurse - Midwife'),
+    ('Nurse - Community health', 'Nurse - Community health'),
+    ('Nurse - Other type (specify)', 'Nurse - Other type (specify)'),
+    ('Nurse - RN', 'Nurse - RN')
+)
