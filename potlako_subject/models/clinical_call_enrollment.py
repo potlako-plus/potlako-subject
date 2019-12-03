c2542eb1d85a27e30362ef79f43c6d22387a9563
tumiemosweu <imosweuk@gmail.com>
2019-12-03 16:01:50 +0200

    PEP8

--- potlako_subject/models/clinical_call_enrollment.py
+++ potlako_subject/models/clinical_call_enrollment.py
@@ -2,22 +2,19 @@ from django.db import models
 
 
 class ClinicalCallEnrollment(models.Model):
-    
+
     record_id = models.CharField(
-        verbose_name="Record Id",
+        verbose_name='Record Id',
         max_length=50,
         unique=True)
-    
+
     start_time = models.DateTimeField(
-        verbose_name="Clinical Enrollment Call: Start Time")
-    
+        verbose_name='Clinical Enrollment Call: Start Time')
+
     date = models.DateField(
-        verbose_name="Date of communication of patient to coordinator")
-    
+        verbose_name='Date of communication of patient to coordinator')
+
     clinician = models.CharField(
-        verbose_name=   "Name of clinician spoken to on the phone "
-                        "for initial call")
-    
-    
-    
-    
+        verbose_name=('Name of clinician spoken to on the phone '
+                      'for initial call')
+    )
