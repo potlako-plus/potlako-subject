b27895fb8ac46391b4765e55688e00b5add0bdb2
amediphoko <diphokoame@gmail.com>
2019-12-05 13:29:45 +0200

    adding admin urls

--- potlako_subject/urls.py
+++ potlako_subject/urls.py
@@ -1,6 +1,11 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls.conf import path
+from django.views.generic.base import RedirectView
+
+from .admin_site import potlako_subject_admin
+
+app_name = 'potlako_subject'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', potlako_subject_admin.urls),
+    path('', RedirectView.as_view(url='admin/'), name='home_url'),
 ]
