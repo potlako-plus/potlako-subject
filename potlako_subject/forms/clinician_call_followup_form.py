06edc7185fd05812ec0a88fdfa60113abd82ae3d
tumiemosweu <imosweuk@gmail.com>
2020-05-07 16:54:38 +0200

    add validators

--- potlako_subject/forms/clinician_call_followup_form.py
+++ potlako_subject/forms/clinician_call_followup_form.py
@@ -1,12 +1,12 @@
 from django import forms
-
+from potlako_validations.form_validators import ClinicianCallEnrollmentFormValidator
 from ..models import ClinicianCallFollowUp
 from .form_mixins import SubjectModelFormMixin
 
 
 class ClinicianCallFollowUpForm(SubjectModelFormMixin, forms.ModelForm):
 
-    # form_validator_cls = ClinicianCallEnrollmentFormValidator
+    form_validator_cls = ClinicianCallEnrollmentFormValidator
 
     class Meta:
         model = ClinicianCallFollowUp
--- potlako_subject/forms/home_visit_form.py
+++ potlako_subject/forms/home_visit_form.py
@@ -1,12 +1,12 @@
 from django import forms
-
+from potlako_validations.form_validators import HomeVisitFormValidator
 from ..models import HomeVisit
 from .form_mixins import SubjectModelFormMixin
 
 
 class HomeVisitForm(SubjectModelFormMixin, forms.ModelForm):
 
-    # form_validator_cls = HomeVisitFormValidator
+    form_validator_cls = HomeVisitFormValidator
 
     class Meta:
         model = HomeVisit
