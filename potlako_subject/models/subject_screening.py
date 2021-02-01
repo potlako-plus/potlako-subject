0d1868e9ef1494f1678007afcc47148ff5f82727
amediphoko <diphokoame@gmail.com>
2021-02-01 10:41:32 +0200

    add search slug mixin to clinician call enrollment

--- potlako_subject/models/subject_screening.py
+++ potlako_subject/models/subject_screening.py
@@ -102,7 +102,7 @@ class SubjectScreening(
     is_consented = models.BooleanField(
         default=False,
         editable=False)
-    
+
     history = HistoricalRecords()
 
     on_site = CurrentSiteManager()
