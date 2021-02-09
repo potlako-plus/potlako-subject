9c7da4f4034137a1a0515dcfbf1c398f0c1c6bc2
tumiemosweu <imosweuk@gmail.com>
2021-02-09 16:21:31 +0200

    make navigation plan form readonly for RAs

--- potlako_subject/admin/navigation_summary_and_plan_admin.py
+++ potlako_subject/admin/navigation_summary_and_plan_admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from edc_model_admin import TabularInlineMixin
-from edc_model_admin import audit_fieldset_tuple
+from edc_model_admin import audit_fieldset_tuple, ModelAdminReadOnlyMixin
 from ..admin_site import potlako_subject_admin
 from ..forms import NavigationSummaryAndPlanForm, EvaluationTimelineForm
 from ..models import NavigationSummaryAndPlan, EvaluationTimeline
@@ -22,9 +22,15 @@ class EvaluationTimelineInlineAdmin(TabularInlineMixin, admin.TabularInline):
                 'review_required')}
          ),)
 
+    def has_change_permission(self, request, obj):
+        return not request.GET.get('edc_readonly')
+
+    def has_add_permission(self, request, obj):
+        return not request.GET.get('edc_readonly')
+
 
 @admin.register(NavigationSummaryAndPlan, site=potlako_subject_admin)
-class NavigationPlanAndSummaryAdmin(ModelAdminMixin, admin.ModelAdmin):
+class NavigationPlanAndSummaryAdmin(ModelAdminMixin, ModelAdminReadOnlyMixin, admin.ModelAdmin):
 
 
     form = NavigationSummaryAndPlanForm
