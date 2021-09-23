2b6f54a73c6b79298529711ef2263bc97ddee625
Itumeleng Mosweu <imosweuk@gmail.com>
2021-09-23 18:03:22 +0200

    update lab test inline

--- potlako_subject/models/investigations_ordered.py
+++ potlako_subject/models/investigations_ordered.py
@@ -141,7 +141,7 @@ class LabTest(SiteModelMixin, BaseUuidModel):
     investigations = models.ForeignKey(InvestigationsOrdered, on_delete=PROTECT)
 
     lab_test_type = models.CharField(
-        verbose_name='Lab test status.',
+        verbose_name='Type of lab test.',
         choices=LAB_TESTS,
         max_length=25,
         help_text='(IF PATIENT CALL, ONLY ASK ABOUT RFT, FBC, LFT )')
@@ -156,7 +156,7 @@ class LabTest(SiteModelMixin, BaseUuidModel):
         null=True)
 
     lab_test_status = models.CharField(
-        verbose_name='Type of lab test.',
+        verbose_name='Lab test status.',
         choices=LAB_TESTS_STATUS,
         max_length=50)
 
