e38dbb81b84c9a2661d0b6057d2a107aaf3c6af9
samKenpachi011 <samuelkabelo1@gmail.com>
2024-04-09 10:43:54 +0200

    UPDATE: trigger action item for visit 3000 for default navigation plan

--- potlako_subject/models/signals.py
+++ potlako_subject/models/signals.py
@@ -470,7 +470,11 @@ def trigger_navigation_summary_reminder(instance):
     try:
         obj = NavigationSummaryAndPlan.objects.get(subject_identifier=instance.subject_identifier)
     except NavigationSummaryAndPlan.DoesNotExist:
-        pass
+        navigation_plan_cls = django_apps.get_model(
+                'potlako_subject.navigationsummaryandplan')
+        trigger_action_item(instance, 'appt_status', 'done',
+                            navigation_plan_cls, NAVIGATION_PLANS_ACTION,
+                            instance.subject_identifier)
     else:
         is_modified = obj.created > obj.modified
         if is_modified:
